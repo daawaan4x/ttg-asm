c494025e43b2c9f7d0121c62ff26914081bfba35
daawaan4x <theoneeclarin@outlook.com>
2025-04-28 15:00:54 +0800

    feat: add print table function

--- .c-port/main.c
+++ .c-port/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <conio.h>
 
+#include ".\table.c"
+
 int main() {
-	printf("Hello World!");
+	
+	char* variables[] = {"P", "Q"};
+	int values[] = {
+		1, 1, 
+		1, 0, 
+		0, 1,
+		0, 0,
+	};
+
+	print_table(
+		4,
+		2,
+		variables,
+		values
+	);
+
 	getch();
 	return 0;
 }
